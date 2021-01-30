49a29d51ade7ce56113ead10cb4a2b84886a63ea
Seongwon97 <osw970325@naver.com>
2021-01-30 23:31:32 +0900

    Network Ex1_2

---
+++ Network1-2/src/myPackage/DateClient.java
@@ -0,0 +1,13 @@
+package myPackage;
+import java.util.Scanner;
+import java.net.Socket;
+import java.io.IOException;
+
+public class DateClient {
+	public static void main(String[] args) throws IOException {
+		
+		Socket socket = new Socket("localhost", 59090);
+		Scanner in = new Scanner(socket.getInputStream());
+		System.out.println("Server response: " + in.nextLine());
+	}
+}
---
+++ Network1-2/src/myPackage/DateServer.java
@@ -0,0 +1,24 @@
+package myPackage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class DateServer {
+
+   public static void main(String[] args) throws IOException{
+      // TODO Auto-generated method stub
+      ServerSocket listener = new ServerSocket(59090);
+      System.out.println("The date server is running...");
+      while(true)
+      {
+         try(Socket socket = listener.accept())
+         {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            out.println(new Date().toString());
+         }
+      }
+
+   }
+}
