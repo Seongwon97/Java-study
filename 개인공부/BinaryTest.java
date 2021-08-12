4a35d623fdc10b78d9dbe9949e6ce4098b6ee595
Seongwon97 <osw970325@naver.com>
2021-08-12 22:30:06 +0900

    Create BinaryTest.java

---
+++ 개인공부/BinaryTest.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		int bNum = 0B1010; //0B를 앞에 붙이면 2진수
+		int oNum = 012; //수 앞에 0을 넣으면 8진수
+		int xNum = 0XA; // 0X를 붙이면 16진수
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+		
+	}
+
+}
