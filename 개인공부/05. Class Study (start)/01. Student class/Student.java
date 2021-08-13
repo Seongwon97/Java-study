5178bfffe8c544dee7f7bb7b1735b5b0a6721283
Seongwon97 <osw970325@naver.com>
2021-08-13 22:14:45 +0900

    Create Student.java

---
+++ 개인공부/05. Class Study (start)/01. Student class/Student.java
@@ -0,0 +1,10 @@
+package ch01;
+
+// java 파일 하나에 클래스는 여러 개가 있을 수 있지만, public 클래스는 하나이고, public 클래스와 .java 파일의 이름은 동일함
+public class Student {
+	int studentNumber;
+	String studentName; //string은 java에서 지원하는 class이다.
+	int majorCode;
+	String majorName;
+	int grade;
+}
