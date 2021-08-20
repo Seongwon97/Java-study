c8ee1a915c56369b73bf439a74c7a040e7392dce
Seongwon97 <osw970325@naver.com>
2021-08-20 16:11:06 +0900

    Update MemberArrayList.java
    Add iterator

--- 개인공부/45. List Interface (Ex)/MemberArrayList.java
+++ 개인공부/45. List Interface (Ex)/MemberArrayList.java
@@ -1,6 +1,7 @@
 package ch10;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
 	// member를 arraylist로 관리하기 위해 만든 class
@@ -21,7 +22,7 @@ public class MemberArrayList {
 	
 	
 	public boolean removeMember(int memberId){  // 멤버 아이디를 매개변수로, 삭제 여부를 반환
-		
+		/*
 		for(int i =0; i<arrayList.size(); i++){ // 해당 아이디를 가진 멤버를 ArrayList에서 찾음
 			Member member = arrayList.get(i);
 			int tempId = member.getMemberId();
@@ -30,6 +31,22 @@ public class MemberArrayList {
 				return true;                   // true 반환
 			}
 		}
+		*/
+		
+		Iterator<Member> ir = arrayList.iterator();
+		// <>안에는 반복 할 자료형을 입력
+		while(ir.hasNext()) {
+			Member member = ir.next();
+			
+			int tempId = member.getMemberId();
+			if(tempId == memberId){
+				arrayList.remove(member); 
+				// 위의 코드에서는 index를 지정하여 remove한 코드
+				// 현재 코드는 iterator를 member object를 사용하기에 object를 빈칸에 입력
+				return true;                  
+			}
+		}
+		
 		
 		System.out.println(memberId + "가 존재하지 않습니다");  //for 가 끝날때 까지 return 이 안된경우
 		return false;                   
