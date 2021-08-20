dfed45c14735df3057e83b6675be64ac20f39d70
Seongwon97 <osw970325@naver.com>
2021-08-20 23:15:27 +0900

    Create 54. Anonymous Inner Class

---
+++ 개인공부/54. Anonymous Inner Class/AnonymousInnerTest.java
@@ -0,0 +1,53 @@
+package ch02_1;
+
+class Outer {
+	int outNum = 100;
+	static int sNum = 200;
+	
+	Runnable getRunnable(int i) {
+		// 매개변수 i는 매서드가 호출될 때 stack memory에 생성이 된다.
+		int num = 10;
+		
+		// anonymous inner class
+		return new Runnable() {
+			
+			int localNum = 1000;	
+			
+			@Override
+			public void run() {
+				//num = 200;
+				//i = 100;
+				// 지역변수와 메서드의 매개변수는 내부클래스에게는 상수로 잡혀서 읽기만 가능하고 변경은 불가하다. (final로 잡힘)
+				// 내부 class의 method는 일을 수행하고나면 사라지는데 외부의 지역변수와 매게변수는 계속 stack에 남아있어서 변경이 불가하다. 
+				
+				System.out.println("i =" + i); 
+				System.out.println("num = " +num);  
+				System.out.println("localNum = " +localNum);
+					
+				System.out.println("outNum = " + outNum + "(외부 클래스 인스턴스 변수)");
+				System.out.println("Outter.sNum = " + Outer.sNum + "(외부 클래스 정적 변수)");
+
+			}
+			
+		};
+	}
+	/* 다음과 같이도 사용 가능
+	Runnable runnable = new Runnable() {
+		@Override
+		public void run() {
+			
+		}
+	};
+	*/
+}
+
+public class AnonymousInnerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Outer out = new Outer();
+		Runnable runner = out.getRunnable(10);
+		runner.run();
+	}
+
+}
